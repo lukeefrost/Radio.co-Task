e54b6f84f140aa8680ad2738b7fed57a3124aef1
vagrant <vagrant@homestead>
2018-07-17 15:10:29 +0000

    Set up Podcast Database Migration, andthe Podcast Model associated with it.

---
+++ database/migrations/2018_07_17_142047_create_podcasts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePodcastsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('podcasts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('url');
+            $table->string('title');
+            $table->string('description');
+            $table->integer('episode_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('podcasts');
+    }
+}
